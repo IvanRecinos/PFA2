39cdcbcad310146f0bf334a20fedfcc3476e3c10
IvanRecinos <irecinos2212@gmail.com>
2023-10-14 15:49:55 -0600

    creación de la pestaña catalogo

---
+++ assets/js/catalogo.js
@@ -0,0 +1,84 @@
+function catalogo1(){
+    let catalogo1 = document.getElementById('no1').style.display = 'flex';
+    let menu1 = document.getElementById('opt1').style.backgroundColor = '#FF66C4';
+
+    let catalogo2 = document.getElementById('no2').style.display = 'none';
+    let menu2 = document.getElementById('opt2').style.backgroundColor = 'white';
+
+    let catalogo3 = document.getElementById('no3').style.display = 'none';
+    let menu3 = document.getElementById('opt3').style.backgroundColor = 'white';
+
+    let catalogo4 = document.getElementById('no4').style.display = 'none';
+    let menu4 = document.getElementById('opt4').style.backgroundColor = 'white';
+
+    let catalogo5 = document.getElementById('no5').style.display = 'none';
+    let menu5 = document.getElementById('opt5').style.backgroundColor = 'white';
+}
+
+function catalogo2(){
+    let catalogo1 = document.getElementById('no1').style.display = 'none';
+    let menu1 = document.getElementById('opt1').style.backgroundColor = 'white';
+
+    let catalogo2 = document.getElementById('no2').style.display = 'flex';
+    let menu2 = document.getElementById('opt2').style.backgroundColor = '#FF66C4';
+
+    let catalogo3 = document.getElementById('no3').style.display = 'none';
+    let menu3 = document.getElementById('opt3').style.backgroundColor = ' white';
+
+    let catalogo4 = document.getElementById('no4').style.display = 'none';
+    let menu4 = document.getElementById('opt4').style.backgroundColor = 'white';
+
+    let catalogo5 = document.getElementById('no5').style.display = 'none';
+    let menu5 = document.getElementById('opt5').style.backgroundColor = ' white';
+}
+
+function catalogo3(){
+    let catalogo1 = document.getElementById('no1').style.display = 'none';
+    let menu1 = document.getElementById('opt1').style.backgroundColor = 'white';
+
+    let catalogo2 = document.getElementById('no2').style.display = 'none';
+    let menu2 = document.getElementById('opt2').style.backgroundColor = 'white';
+
+    let catalogo3 = document.getElementById('no3').style.display = 'flex';
+    let menu3 = document.getElementById('opt3').style.backgroundColor = '#FF66C4';
+
+    let catalogo4 = document.getElementById('no4').style.display = 'none';
+    let menu4 = document.getElementById('opt4').style.backgroundColor = 'white';
+
+    let catalogo5 = document.getElementById('no5').style.display = 'none';
+    let menu5 = document.getElementById('opt5').style.backgroundColor = 'white';
+}
+
+function catalogo4(){
+    let catalogo1 = document.getElementById('no1').style.display = 'none';
+    let menu1 = document.getElementById('opt1').style.backgroundColor = 'white';
+
+    let catalogo2 = document.getElementById('no2').style.display = 'none';
+    let menu2 = document.getElementById('opt2').style.backgroundColor = 'white';
+
+    let catalogo3 = document.getElementById('no3').style.display = 'none';
+    let menu3 = document.getElementById('opt3').style.backgroundColor = 'white';
+
+    let catalogo4 = document.getElementById('no4').style.display = 'flex';
+    let menu4 = document.getElementById('opt4').style.backgroundColor = '#FF66C4';
+
+    let catalogo5 = document.getElementById('no5').style.display = 'none';
+    let menu5 = document.getElementById('opt5').style.backgroundColor = 'white';
+}
+
+function catalogo5(){
+    let catalogo1 = document.getElementById('no1').style.display = 'none';
+    let menu1 = document.getElementById('opt1').style.backgroundColor = 'white';
+
+    let catalogo2 = document.getElementById('no2').style.display = 'none';
+    let menu2 = document.getElementById('opt2').style.backgroundColor = 'white';
+
+    let catalogo3 = document.getElementById('no3').style.display = 'none';
+    let menu3 = document.getElementById('opt3').style.backgroundColor = 'white';
+
+    let catalogo4 = document.getElementById('no4').style.display = 'none';
+    let menu4 = document.getElementById('opt4').style.backgroundColor = 'white';
+
+    let catalogo5 = document.getElementById('no5').style.display = 'flex';
+    let menu5 = document.getElementById('opt5').style.backgroundColor = '#FF66C4';
+}
--- routes/direcciones.js
+++ routes/direcciones.js
@@ -9,5 +9,8 @@ router.get('/producto', function(req, res){
     res.render("pages/producto")
 });
 
+router.get('/catalogo', function(req, res){
+    res.render("pages/catalogo")
+});
     
 module.exports = router
